b48234c3167451b2cbab344e21c263ff03319b42
julobeau <julien.beaumel@free.fr>
2024-04-04 00:58:34 +0200

    amelioration interface

--- assets/js/animationInformations.js
+++ assets/js/animationInformations.js
@@ -1,38 +1,46 @@
+let panneauOuvert = false
+
 function MoveIn() {
-    let id = null;
-    const elem = document.getElementById("systeme__informations");
-    let pos = -450;
-    clearInterval(id);
-    id = setInterval(frame, 3);
-    function frame() {
-        if (pos == 0) {
+    if(!panneauOuvert){
+        let id = null;
+        const elem = document.getElementById("systeme__informations");
+        let pos = -450;
         clearInterval(id);
-        } else {
-        pos++;
-        //elem.style.top = pos + 'px';
-        elem.style.left = pos + 'px';
+        id = setInterval(frame, 3);
+        function frame() {
+            if (pos == 0) {
+            clearInterval(id);
+            } else {
+            pos++;
+            //elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+            }
         }
     }
+    panneauOuvert = true
 }
 
 //const btnFermeture = document.getElementById("infos-btn-fermeture");
 
 
 function MoveOut() {
-    let id = null;
-    const elem = document.getElementById("systeme__informations");
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 3);
-    function frame() {
-        if (pos == -450) {
+    if(panneauOuvert){
+        let id = null;
+        const elem = document.getElementById("systeme__informations");
+        let pos = 0;
         clearInterval(id);
-        } else {
-        pos--;
-        //elem.style.top = pos + 'px';
-        elem.style.left = pos + 'px';
+        id = setInterval(frame, 3);
+        function frame() {
+            if (pos == -450) {
+            clearInterval(id);
+            } else {
+            pos--;
+            //elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+            }
         }
     }
+    panneauOuvert = false
 }
 
 document.getElementById("infos-btn-fermeture").addEventListener('click', () => {
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,11 +1,30 @@
 const planets_list = document.querySelectorAll(".planet")
 const sun = document.getElementById("soleil")
 
+function addSelectionFX(body){
+    const bodyActif = document.getElementById(body)
+    bodyActif.style.filter = "drop-Shadow(0 0 7px red)"
+    document.getElementById("asteroidBelt").style.filter = ""
+}
+
+function removeSelectionFX() {
+    sun.style.filter = ""
+    for (let planet of planets_list){
+        document.getElementById(planet.id).style.filter = ""
+    }
+}
 
 function displayProperties(body) {
     document.getElementById("nomBody").innerText = body.name
+    let type = ''
+    if(body.bodyType == "Star"){
+        document.getElementById("bodyTypeValue").innerText = "Etoile"
+    }else{
+        document.getElementById("bodyTypeValue").innerText = "Planète"
+    }
     document.getElementById("bodySizeValue").innerText = `${body.meanRadius} km`
     document.getElementById("bodyMassValue").innerText = `${body.mass.massValue} 10^${body.mass.massExponent}`
+
     MoveIn()
 }
 
@@ -14,15 +33,6 @@ async function getBodyData(body, callback) {
         method: "GET",
         redirect: "follow"
     };
-
-    /*try {
-        const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${body}`, requestOptions)
-        const data = await response.json()
-        callback(data)
-    }
-    catch {
-        console.log("error")
-    }*/
 
     fetch("https://api.le-systeme-solaire.net/rest/bodies/"+body, requestOptions)
         .then((response) => response.json())
@@ -36,11 +46,14 @@ async function getBodyData(body, callback) {
 
 sun.addEventListener("click", () => {
     getBodyData(sun.id, displayProperties)
+    removeSelectionFX()
 })
 
 
 for (let planet of planets_list){
     planet.addEventListener("click", () => {
         getBodyData(planet.id, displayProperties)
+        removeSelectionFX()
+        addSelectionFX(planet.id)
     })
 }
